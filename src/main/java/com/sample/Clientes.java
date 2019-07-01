d5e7830ded733dce75cfc6fa930a1dc55bc1cd42
jonasmigas <34393594+jonasmigas@users.noreply.github.com>
2019-07-01 17:40:28 +0100

    Initial commit

---
+++ src/main/java/com/sample/Clientes.java
@@ -0,0 +1,32 @@
+package com.sample;
+
+public class Clientes {
+	String nome;
+	String sexo;
+	String estado_civil;
+	Boolean filhos;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEstado_civil() {
+		return estado_civil;
+	}
+	public void setEstado_civil(String estado_civil) {
+		this.estado_civil = estado_civil;
+	}
+	public Boolean getFilhos() {
+		return filhos;
+	}
+	public void setFilhos(Boolean filhos) {
+		this.filhos = filhos;
+	}
+}
---
+++ src/main/java/com/sample/Items.java
@@ -0,0 +1,25 @@
+package com.sample;
+
+public class Items {
+	String nome;
+	String tipo;
+	Integer quant;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Integer getQuant() {
+		return quant;
+	}
+	public void setQuant(Integer quant) {
+		this.quant = quant;
+	}
+}

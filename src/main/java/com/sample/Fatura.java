1bf224601cde6c0a90f36833d5a3bacd4bf66041
jmurteira <46197229+jmurteira@users.noreply.github.com>
2019-07-02 17:20:12 +0100

    atualiza fatura

--- src/main/java/com/sample/Fatura.java
+++ src/main/java/com/sample/Fatura.java
@@ -3,23 +3,37 @@ package com.sample;
 public class Fatura {
 	int nItems;
 	float PrecoTotal;
+	int idAtual;
 	
-	public Fatura(int nItems, int precoTotal) {
+	public Fatura(int nItems, float precoTotal, int idAtual) {
 		super();
 		this.nItems = nItems;
 		PrecoTotal = precoTotal;
+		this.idAtual = idAtual;
 	}
+
 	public int getnItems() {
 		return nItems;
 	}
+
 	public void setnItems(int nItems) {
 		this.nItems = nItems;
 	}
+
 	public float getPrecoTotal() {
 		return PrecoTotal;
 	}
-	public void setPrecoTotal(int precoTotal) {
+
+	public void setPrecoTotal(float precoTotal) {
 		PrecoTotal = precoTotal;
+	}
+
+	public int getIdAtual() {
+		return idAtual;
+	}
+
+	public void setIdAtual(int idAtual) {
+		this.idAtual = idAtual;
 	}
 	
 }

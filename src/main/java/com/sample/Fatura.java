a1321bc76534f6a090cd8263da9282e73a443c26
jmurteira <46197229+jmurteira@users.noreply.github.com>
2019-07-03 12:56:10 +0100

    JÁ FUNFA

--- src/main/java/com/sample/Fatura.java
+++ src/main/java/com/sample/Fatura.java
@@ -4,36 +4,37 @@ public class Fatura {
 	int nItems;
 	float PrecoTotal;
 	int idAtual;
+	boolean camisola;
 	
-	public Fatura(int nItems, float precoTotal, int idAtual) {
+	public Fatura(int nItems, float precoTotal, int idAtual, boolean camisola) {
 		super();
 		this.nItems = nItems;
 		PrecoTotal = precoTotal;
 		this.idAtual = idAtual;
+		this.camisola = camisola;
 	}
-
 	public int getnItems() {
 		return nItems;
 	}
-
 	public void setnItems(int nItems) {
 		this.nItems = nItems;
 	}
-
 	public float getPrecoTotal() {
 		return PrecoTotal;
 	}
-
 	public void setPrecoTotal(float precoTotal) {
 		PrecoTotal = precoTotal;
 	}
-
 	public int getIdAtual() {
 		return idAtual;
 	}
-
 	public void setIdAtual(int idAtual) {
 		this.idAtual = idAtual;
 	}
-	
+	public boolean isCamisola() {
+		return camisola;
+	}
+	public void setCamisola(boolean camisola) {
+		this.camisola = camisola;
+	}
 }

304008deca097674c9c07f76f7117f2e93a15c25
jonasmigas <34393594+jonasmigas@users.noreply.github.com>
2019-07-04 01:33:11 +0100

    não rebenta!

--- src/main/java/com/sample/Fatura.java
+++ src/main/java/com/sample/Fatura.java
@@ -7,9 +7,9 @@ public class Fatura {
 	boolean camisola;
 	boolean complementar;
 	boolean sexoOposto;
-	
-	public Fatura(int nItems, float precoTotal, int idAtual, boolean camisola, boolean complementar,
-			boolean sexoOposto) {
+	boolean naoExiste;
+	public Fatura(int nItems, float precoTotal, int idAtual, boolean camisola, boolean complementar, boolean sexoOposto,
+			boolean naoExiste) {
 		super();
 		this.nItems = nItems;
 		PrecoTotal = precoTotal;
@@ -17,6 +17,7 @@ public class Fatura {
 		this.camisola = camisola;
 		this.complementar = complementar;
 		this.sexoOposto = sexoOposto;
+		this.naoExiste = naoExiste;
 	}
 	public int getnItems() {
 		return nItems;
@@ -54,6 +55,14 @@ public class Fatura {
 	public void setSexoOposto(boolean sexoOposto) {
 		this.sexoOposto = sexoOposto;
 	}
+	public boolean isNaoExiste() {
+		return naoExiste;
+	}
+	public void setNaoExiste(boolean naoExiste) {
+		this.naoExiste = naoExiste;
+	}
+	
+	
 	
 	
 }

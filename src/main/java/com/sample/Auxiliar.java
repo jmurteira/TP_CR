1c07b3955fd5648dd0e10d3ce8065ee937479d2f
jonasmigas <34393594+jonasmigas@users.noreply.github.com>
2019-07-05 14:00:47 +0100

    ja funca

---
+++ src/main/java/com/sample/Auxiliar.java
@@ -0,0 +1,26 @@
+package com.sample;
+
+public class Auxiliar {
+	int idAux;
+	int compraAposCrianca;
+	public Auxiliar(int idAux, int compraAposCrianca) {
+		super();
+		this.idAux = idAux;
+		this.compraAposCrianca = compraAposCrianca;
+	}
+	public int getIdAux() {
+		return idAux;
+	}
+	public void setIdAux(int idAux) {
+		this.idAux = idAux;
+	}
+	public int getCompraAposCrianca() {
+		return compraAposCrianca;
+	}
+	public void setCompraAposCrianca(int compraAposCrianca) {
+		this.compraAposCrianca = compraAposCrianca;
+	}
+	
+
+	
+}

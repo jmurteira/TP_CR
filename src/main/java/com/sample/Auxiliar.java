d41842ae7385e0e9f921cb8925fdb47151392284
jonasmigas <34393594+jonasmigas@users.noreply.github.com>
2019-07-05 15:51:58 +0100

    regras funcionam todas

--- src/main/java/com/sample/Auxiliar.java
+++ src/main/java/com/sample/Auxiliar.java
@@ -3,10 +3,14 @@ package com.sample;
 public class Auxiliar {
 	int idAux;
 	int compraAposCrianca;
-	public Auxiliar(int idAux, int compraAposCrianca) {
+	boolean naoExiste;
+	int guardaUltimoItem;
+	public Auxiliar(int idAux, int compraAposCrianca, boolean naoExiste, int guardaUltimoItem) {
 		super();
 		this.idAux = idAux;
 		this.compraAposCrianca = compraAposCrianca;
+		this.naoExiste = naoExiste;
+		this.guardaUltimoItem = guardaUltimoItem;
 	}
 	public int getIdAux() {
 		return idAux;
@@ -20,7 +24,20 @@ public class Auxiliar {
 	public void setCompraAposCrianca(int compraAposCrianca) {
 		this.compraAposCrianca = compraAposCrianca;
 	}
+	public boolean isNaoExiste() {
+		return naoExiste;
+	}
+	public void setNaoExiste(boolean naoExiste) {
+		this.naoExiste = naoExiste;
+	}
+	public int getGuardaUltimoItem() {
+		return guardaUltimoItem;
+	}
+	public void setGuardaUltimoItem(int guardaUltimoItem) {
+		this.guardaUltimoItem = guardaUltimoItem;
+	}
+	
+	
 	
 
-	
 }
